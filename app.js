670cb41b183649ed2d28ae16cc33f5215d2a1996
kamal elazizi alaoui <elazizialaouikamal@yahoo.com>
2017-12-10 00:07:05 -0500

    bon11

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var query = require('./query');
 var path = require('path');
 var pug = require('pug');
 var bodyParser = require('body-parser');
-// load environment variables
-
 //set port
 var port = process.env.PORT || 5000
 const { Client } = require('pg');
@@ -20,10 +18,7 @@ app.set('view engine', 'pug');
 // app.use(express.static('assets'));
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: false}));
-require('dotenv').config();
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //CREATE DATABASE blog;
 // \c blog;
@@ -57,19 +52,19 @@ function get_post (id){
 app.get('/', function(request, response){
 
     client.query('SELECT * FROM posts', (err, res) => {
-                if (err) throw err;
-                let arr = [];
-                for (let row of res.rows) {
-                  arr.push(row);
-                }
-       console.log(arr);
-                // client.end();
-                response.render('index',{
-                      posts: arr,
-                      title: 'Here are all the posts:'
-                 });
-                 client.end();
-              });
+      if (err) throw err;
+      let arr = [];
+      for (let row of res.rows) {
+        arr.push(row);
+      }
+      console.log(arr);
+      // client.end();
+      response.render('index',{
+        posts: arr,
+        title: 'Here are all the posts:'
+       });
+       client.end();
+    });
 
 // app.get('/', function(request, response){
 //           get_post().then(function(posts){
@@ -106,19 +101,21 @@ app.post('/add-post', function(req, res){
   // console.log(req.query.title);
     query('insert into posts (title, excerpt, body) values ($1, $2, $3)', [req.query.title, req.query.username , req.query.message], function(err, results){
    //handle the error and results as appropriate.
-             // if(err){
-             //  console.log(err);
-             //  // return done(client);
-             //  }
-             //  return done(client);
-             // console.log('New Post accepted.');
-             //  });
+             if(err){
+              console.log(err);
+              // return done(client);
+              }
+              return done(client);
+             console.log('New Post accepted.');
+              });
              return res.redirect('/');
     });
+
 
 app.get('*', function(req, res) {
   res.status(404).send('<h1>uh oh! page not found!</h1>');
 });
+
 
 // var server = app.listen(3333, function(){
 //   console.log('Open http://localhost:3333 in the browser');
@@ -128,4 +125,4 @@ app.get('*', function(req, res) {
   //   });
   app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-             });
+});
