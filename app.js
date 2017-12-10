dcabc646d71e186321d67f61ab91de8dd1179628
kamal elazizi alaoui <elazizialaouikamal@yahoo.com>
2017-12-09 23:47:41 -0500

    adc

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(expressValidator());
 
 //CREATE DATABASE blog;
