c2514a433ac28a2e87b53e893d749351bd33b495
kamal elazizi alaoui <elazizialaouikamal@yahoo.com>
2017-12-10 00:30:10 -0500

    alla1

--- app.js
+++ app.js
@@ -124,6 +124,6 @@ app.get('*', function(req, res) {
   // app.listen(port, function() {
   //           console.log('app running');
   //   });
-  app.listen(process.env.PORT || 3000, function(){
+  app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
